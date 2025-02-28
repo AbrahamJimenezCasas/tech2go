d96f2b640cb98937c9f399fc9b19985ba1153c7e
Ester Castarnado <castarnado@hotmail.com>
2025-02-28 16:43:40 +0100

    cambio-nombre

--- src/pages/PublishProductPage.jsx
+++ src/pages/PublishProductPage.jsx
@@ -3,7 +3,7 @@ import { PublishProductForm } from "../components/forms/PublishProductForm.jsx";
 export const PublishProductPage = () => {
     return <section className="flex flex-col items-center p-6 lg:px-32 2xl:px-40 lg:py-8 min-h-[calc(100svh-5rem)]">
     <h2 className="font-display text-electric-violet-800 text-4xl">
-        Inicia sesión
+        Vende tu artículo
     </h2>
         <PublishProductForm/>
 
