f9fff7405e5248b08c98cd2aeffc8e396d89b75b
Ester Castarnado <castarnado@hotmail.com>
2025-03-02 02:08:43 +0100

    Cambiosesteticos

--- src/components/products/ProductDetail.jsx
+++ src/components/products/ProductDetail.jsx
@@ -1,27 +1,24 @@
  export const ProductDetail = ({ product }) => {
   return (
-    <section className="md:flex">
-      <article className="md:shrink-0">
+    <section className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-10">
+      <article className="md:w-1/3 bg-gray-200 flex items-center justify-center p-6">
         <img
           className="h-48 w-full object-cover md:h-full md:w-48"
           src={product.imageUrl}
           alt={product.nombre}
         ></img>
       </article>
-      <article className="p-8">
-        
-        
-        
-        <p className="block mt-1 text-lg leading-tight font-medium text-black">
-          Nombre {product.nombre}
+      <article className="p-8">                
+        <p className="text-lg leading-tight  text-electric-violet-500">
+          Nombre: <p className="mt-1 text-black text-sm"> {product.nombre}</p>
         </p>
-        <article className="uppercase tracking-wide text-sm text-electric-violet-500 font-semibold">
-        Categoria {product.categoria}
+        <article className="mt-1 text-lg leading-tight text-electric-violet-500">
+          Categoria: <p className ="mt-1 text-black text-sm"> {product.categoria}</p>
         </article>
-        <p className="mt-2 text-dark">
-          Descripción {product.descripcion}
+        <p className="text-lg leading-tight text-electric-violet-500">
+          Descripción: {product.descripcion}
         </p>
-        <p className="mt-2 text-slate-900 font-bold">
+        <p className="mt-2 text-slate-900">
           Precio: {product.precio} €
         </p>
         <p className="mt-2 text-slate-500">
