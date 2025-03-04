e97d5c8fe31bee19b8b3fca310a0afec8cc8add6
Sara Olveira <93060063+saraolveira@users.noreply.github.com>
2025-03-04 16:03:05 +0100

    Diseño aceptar/rechazar solicitud

--- src/pages/AcceptRejectBuyRequestPage.jsx
+++ src/pages/AcceptRejectBuyRequestPage.jsx
@@ -4,7 +4,6 @@ import { useProduct } from "../hooks/useProduct.js";
 import { useUser } from "../hooks/useUser.js";
 import { useRequest } from "../hooks/useRequest.js";
 import { RateProductCard } from "../components/products/RateProductCard.jsx";
-import { formatDate } from "../utils/dayJs.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faSquareCheck,
@@ -20,7 +19,8 @@ export const AcceptRejectBuyRequestPage = () => {
     const { product } = useProduct(id);
     const { user } = useUser(null, token);
     const { request } = useRequest(id, id_sol, token);
-    const [loading, setIsLoading] = useState();
+    const [loading, setIsLoading] = useState(false);
+    const [finalIcon, setFinalIcon] = useState(null);
     // console.log(request);
     // console.log(product?.vendedorId === user?.id);
 
@@ -30,6 +30,8 @@ export const AcceptRejectBuyRequestPage = () => {
             const estado = { estado: accion };
 
             await updateBuyRequestStateService(id, id_sol, token, estado);
+
+            setFinalIcon(accion === "aceptada" ? faSquareCheck : faSquareXmark);
 
             toast.success(`Solicitud ${accion} con éxito`);
         } catch (error) {
@@ -61,20 +63,29 @@ export const AcceptRejectBuyRequestPage = () => {
                                 className="text-electric-violet-800 text-4xl text"
                             />
                         )}
-                        {request.estado === "pendiente" && (
-                            <div className="flex justify-center gap-4">
-                                <FontAwesomeIcon
-                                    icon={faSquareXmark}
-                                    className="text-electric-violet-500 hover:text-electric-violet-800 text-5xl transition-colors duration-200 cursor-pointer text"
-                                    name="rechazada"
-                                    onClick={() => handleClick("rechazada")}
-                                />
-                                <FontAwesomeIcon
-                                    icon={faSquareCheck}
-                                    className="text-electric-violet-500 hover:text-electric-violet-800 text-5xl transition-colors duration-200 cursor-pointer text"
-                                    onClick={() => handleClick("aceptada")}
-                                />
-                            </div>
+                        {request.estado === "pendiente" &&
+                            product?.vendedorId === user?.id && (
+                                <div
+                                    className={`${finalIcon ? "hidden" : "flex"} justify-center gap-4`}
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faSquareXmark}
+                                        className="text-electric-violet-500 hover:text-electric-violet-800 text-5xl transition-colors duration-200 cursor-pointer text"
+                                        name="rechazada"
+                                        onClick={() => handleClick("rechazada")}
+                                    />
+                                    <FontAwesomeIcon
+                                        icon={faSquareCheck}
+                                        className="text-electric-violet-500 hover:text-electric-violet-800 text-5xl transition-colors duration-200 cursor-pointer text"
+                                        onClick={() => handleClick("aceptada")}
+                                    />
+                                </div>
+                            )}
+                        {finalIcon && (
+                            <FontAwesomeIcon
+                                icon={finalIcon}
+                                className="text-electric-violet-800 text-4xl text"
+                            />
                         )}
                     </div>
                 )}
