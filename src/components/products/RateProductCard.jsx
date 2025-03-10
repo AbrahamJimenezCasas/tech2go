01d04a4f6b1e8104bbc1ed13e4f3043f83517236
Sara Olveira <93060063+saraolveira@users.noreply.github.com>
2025-03-10 16:19:41 +0100

    Imagenes optimizadas

--- src/components/products/RateProductCard.jsx
+++ src/components/products/RateProductCard.jsx
@@ -1,25 +1,30 @@
+import { Link } from "react-router-dom";
+
 export const RateProductCard = ({ product }) => {
     const staticPath = import.meta.env.VITE_BACKEND_STATIC;
     const fotos = product.fotos;
     const foto = fotos ? fotos[0] : null;
 
     return (
-        <div className="flex flex-col items-center bg-light shadow-dark/10 shadow-lg p-4 rounded-2xl w-full sm:w-1/3">
+        <Link
+            to={`/articulos/${product.id}`}
+            className="group flex flex-col items-center bg-light shadow-dark/10 shadow-lg hover:shadow-electric-violet-800/10 p-4 rounded-2xl w-full sm:w-1/3"
+        >
             <img
                 src={
                     foto
                         ? `${staticPath}/articulos/${product.vendedorId}/${product.id}/${foto.foto}`
                         : "/imgs/no-img-available.png"
                 }
-                className="shadow-lg rounded-2xl w-full object-cover"
+                className="shadow-lg group-hover:shadow-electric-violet-800/10 rounded-2xl w-full object-cover transition-all group-hover:-translate-y-1 duration-200"
                 alt={`Imagen de ${product.nombre}`}
             />
-            <p className="mt-4 mb-2 w-full font-body font-bold text-electric-violet-950 text-center leading-none">
+            <p className="group-hover:text-electric-violet-800 mt-4 mb-2 w-full font-body font-bold text-electric-violet-950 text-center leading-none transition-colors duration-200">
                 {product.nombre}
             </p>
             <p className="w-full font-body font-light text-electric-violet-950 text-center leading-none">
                 {product.precio} €
             </p>
-        </div>
+        </Link>
     );
 };
